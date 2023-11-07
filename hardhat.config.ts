1dc07360afb3f873c7e63ca3a90a488137e872d2
ocularzenith <jywbawlo@gmail.com>
2023-11-07 03:52:15 +0100

    update api key injection

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
+require('dotenv').config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -12,7 +14,7 @@ const config: HardhatUserConfig = {
     },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: ["${process.env.DEPLOYER_PRIV_KEY}"]
+      accounts: [`${process.env.DEPLOYER_PRIV_KEY}`]
 
     }
   }
